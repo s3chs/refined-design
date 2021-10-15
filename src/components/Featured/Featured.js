5446c86888af7a072dc4387e30fdaea20d7b4e63
sechs <philippe.buteau@skema.edu>
2021-10-15 11:34:31 +0200

    header and featured section done

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import "./featured.scss"
+import React from "react";
+import "./featured.scss";
+import photos from "../../data";
 
 export default function Featured() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [firstUrl, secondUrl] = photos;
+
+  return (
+    <section className="featured-section">
+      <div className="featured-row-layout">
+        <h6>green</h6>
+        <img src={firstUrl} />
+      </div>
+      <div className="featured-column-layout">
+        <h6>lily</h6>
+        <img src={secondUrl} />
+      </div>
+    </section>
+  );
 }
